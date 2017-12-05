fb53c033f917ff194c27682a841e17c0fc3c2936
user <user@Computer.local>
2017-12-04 21:30:42 -0700

    Clean up some code

--- src/conway.elm
+++ src/conway.elm
@@ -1,19 +1,18 @@
 module Conway exposing (..)
 
 import AnimationFrame
-import Board exposing (Board, Status(Dead, Alive), Point)
+import Board exposing (Board, Status(..), Point)
 import Debug
 import Html exposing (..)
 import Html.Events exposing (onClick, onMouseDown, onMouseUp, onMouseOver)
 import Html.Attributes exposing (style, class, classList)
-import Task
 import Time exposing (Time)
 
 
 main : Program Never Model Msg
 main =
     program
-        { init = init
+        { init = ( defaultModel, Cmd.none )
         , view = view
         , update = update
         , subscriptions = subscriptions
@@ -22,12 +21,12 @@ main =
 
 type Msg
     = NoOp
-    | Tick Time
     | Frame Time
-    | Toggle Point Status
+    | StepGame
+    | SetPoint Point Status
     | ResetBoard
     | PlayPause
-    | Dragging Bool
+    | SetDragging Bool
 
 
 type alias Model =
@@ -53,47 +52,83 @@ defaultModel =
     }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( defaultModel, Cmd.none )
+
+-- View
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ boardTable model <| Board.toList model.board
-        , span [ class "iterations" ] [ text <| toString model.iterations ]
-        , button [ onClick ResetBoard ] [ text "Reset" ]
-        , button [ onClick (Tick 0) ] [ text "Next" ]
-        , button [ onClick PlayPause ]
-            [ text <|
-                if model.paused then
-                    "Run"
-                else
-                    "Stop"
-            ]
+        [ gameBoard model
+        , iterations model.iterations
+        , resetButton
+        , nextButton
+        , playPauseButton model.paused
         ]
 
 
-boardTable : Model -> List (List Status) -> Html Msg
+iterations : Int -> Html Msg
+iterations iterations =
+    span [ class "iterations" ] [ text <| toString iterations ]
+
+
+resetButton : Html Msg
+resetButton =
+    button [ onClick ResetBoard ] [ text "Reset" ]
+
+
+nextButton : Html Msg
+nextButton =
+    button [ onClick StepGame ] [ text "Next" ]
+
+
+playPauseButton : Bool -> Html Msg
+playPauseButton paused =
+    button [ onClick PlayPause ]
+        [ text <|
+            if paused then
+                "Run"
+            else
+                "Stop"
+        ]
+
+
+
+-- Display Board Game
+
+
+type alias BoardModel a =
+    { a
+        | board : Board
+        , cellSize : Int
+        , dragging : Bool
+    }
+
+
+gameBoard : BoardModel a -> Html Msg
+gameBoard model =
+    Board.toList model.board
+        |> boardTable model
+
+
+boardTable : BoardModel a -> List (List Status) -> Html Msg
 boardTable model list =
     table
-        [ classList
-            [ ( "table", True ) ]
-        , onMouseDown (Dragging True)
-        , onMouseUp (Dragging False)
+        [ class "table"
+        , onMouseDown (SetDragging True)
+        , onMouseUp (SetDragging False)
         ]
         (List.indexedMap (boardRow model) list)
 
 
-boardRow : Model -> Int -> List Status -> Html Msg
+boardRow : BoardModel a -> Int -> List Status -> Html Msg
 boardRow model x row =
     tr
-        [ classList [ ( "row", True ) ] ]
+        [ class "row" ]
         (List.indexedMap (\y cell -> boardCell model ( x, y ) cell) row)
 
 
-boardCell : Model -> Point -> Status -> Html Msg
+boardCell : BoardModel a -> Point -> Status -> Html Msg
 boardCell model point status =
     td
         [ classList
@@ -110,17 +145,21 @@ boardCell model point status =
 
         -- Toggle the cell status on click
         , onClick <|
-            Toggle point <|
+            SetPoint point <|
                 Board.toggle status
 
         -- Make cell alive on hover if in the middle of dragging
         , onMouseOver <|
             if model.dragging then
-                Toggle point Alive
+                SetPoint point Alive
             else
                 NoOp
         ]
         []
+
+
+
+-- Update
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -129,60 +168,98 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
-        -- Triggered on every animation frame
-        -- Once enough time has elapsed trigger the next application tick
-        Frame dt ->
+        Frame delta ->
             if model.paused then
                 ( model, Cmd.none )
-            else if model.elapsed >= model.tickThreshold then
-                ( model, Task.perform Tick Time.now )
+            else if shouldStep model then
+                ( stepGame model, Cmd.none )
             else
-                ( { model | elapsed = model.elapsed + dt }
-                , Cmd.none
-                )
+                ( timeElapsed delta model, Cmd.none )
 
-        -- Advance the application by one step
-        Tick _ ->
-            ( { model
-                | board = Board.nextBoard model.board
-                , iterations = model.iterations + 1
-                , elapsed = 0
-              }
+        StepGame ->
+            ( stepGame model, Cmd.none )
+
+        SetPoint point status ->
+            ( model
+                |> setPoint point status
+                |> setPause True
             , Cmd.none
             )
 
-        -- Toggle the state of a given cell
-        -- The game is paused to allow further modification
-        Toggle point status ->
-            ( { model
-                | board = Board.set model.board point status
-                , paused = True
-              }
-            , Cmd.none
-            )
-
-        -- Reset the board and some additional state
         ResetBoard ->
-            ( { model
-                | board = Board.clear model.board
-                , iterations = 0
-                , elapsed = 0
-                , paused = True
-              }
+            ( model
+                |> resetBoard
+                |> setPause True
             , Cmd.none
             )
 
-        -- Start / Stop dragging so we can toggle cells with only one click
-        Dragging bool ->
-            ( { model | dragging = bool }
-            , Cmd.none
-            )
+        SetDragging bool ->
+            ( setDragging bool model, Cmd.none )
 
-        -- Toggle Play/Pause state
         PlayPause ->
-            ( { model | paused = not model.paused }
-            , Task.perform Tick Time.now
+            ( model
+                |> togglePause
+                |> stepGame
+            , Cmd.none
             )
+
+
+setDragging : Bool -> Model -> Model
+setDragging dragging model =
+    { model
+        | dragging = dragging
+    }
+
+
+setPoint : Point -> Status -> Model -> Model
+setPoint point status model =
+    { model
+        | board = Board.set model.board point status
+    }
+
+
+resetBoard : Model -> Model
+resetBoard model =
+    { model
+        | board = Board.clear model.board
+        , iterations = 0
+        , elapsed = 0
+    }
+
+
+stepGame : Model -> Model
+stepGame model =
+    { model
+        | board = Board.nextBoard model.board
+        , iterations = model.iterations + 1
+        , elapsed = 0
+    }
+
+
+shouldStep : Model -> Bool
+shouldStep model =
+    model.elapsed >= model.tickThreshold
+
+
+timeElapsed : Float -> Model -> Model
+timeElapsed delta model =
+    { model
+        | elapsed = model.elapsed + delta
+    }
+
+
+togglePause : Model -> Model
+togglePause model =
+    setPause (not model.paused) model
+
+
+setPause : Bool -> Model -> Model
+setPause pause model =
+    { model | paused = pause }
+
+
+
+-- Subscriptions
 
 
 subscriptions : Model -> Sub Msg
